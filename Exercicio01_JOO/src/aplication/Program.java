4f7aa7ece6f7387ea5ff8c5d40800955b93dab8e
Alex <alexsandrodp1@gmail.com>
2023-08-30 11:04:45 -0300

    adicionado comentario

--- Exercicio01_JOO/src/aplication/Program.java
+++ Exercicio01_JOO/src/aplication/Program.java
@@ -8,6 +8,13 @@ import entities.Rectangle;
 public class Program {
 
 	public static void main(String[] args) {
+		/*
+		 * Fazer um programa para ler os valores da largura e altura
+de um retângulo. Em seguida, mostrar na tela o valor de
+sua área, perímetro e diagonal. Usar uma classe como
+mostrado no projeto ao lado.
+		 */
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
