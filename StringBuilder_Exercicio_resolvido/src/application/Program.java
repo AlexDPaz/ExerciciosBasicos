36957f2d792d1ffa56b4290d7e16398f99030dd2
Alex <alexsandrodp1@gmail.com>
2023-11-04 21:48:52 -0300

    Add StringBuilder

--- StringBuilder_Exercicio_resolvido/src/application/Program.java
+++ StringBuilder_Exercicio_resolvido/src/application/Program.java
@@ -1,10 +1,25 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import entities.Comment;
+import entities.Post;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		for(int i=0; i<=2; i++) {
+		Comment coment = new Comment("Commentite");
+		String text1 = sc.nextLine();
+		Post post = new Post(mo, text1, text1, null)
+		}
+		
+		sc.close();
 	}
 
 }
