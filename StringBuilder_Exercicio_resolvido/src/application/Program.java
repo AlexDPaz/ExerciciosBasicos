7dd5a812b01714c65e5dfb98b30a38888418d0ff
Alex <alexsandrodp1@gmail.com>
2023-11-04 21:49:42 -0300

    finalizado o programa

--- StringBuilder_Exercicio_resolvido/src/application/Program.java
+++ StringBuilder_Exercicio_resolvido/src/application/Program.java
@@ -1,25 +1,39 @@
 package application;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Scanner;
 
 import entities.Comment;
 import entities.Post;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws ParseException {
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
-		for(int i=0; i<=2; i++) {
-		Comment coment = new Comment("Commentite");
-		String text1 = sc.nextLine();
-		Post post = new Post(mo, text1, text1, null)
-		}
+		Comment c1 = new Comment("Have a nice trip!");
+		Comment c2 = new Comment("Wow that's awesome!");
+		Post p1 = new Post(
+				sdf.parse("21/06/2018 13:05:44"), 
+				"Traveling to New Zealand", 
+				"I'm going to visit this wonderful country!", 
+				12);
+		p1.addComment(c1);
+		p1.addComment(c2);
 		
-		sc.close();
+		Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force be with you");
+		Post p2 = new Post(
+				sdf.parse("28/07/2018 23:14:19"), 
+				"Good night guys", 
+				"See you tomorrow", 
+				5);
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
