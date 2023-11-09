1ce11ed1464e64ba0d005506b30927cdd441d9e5
Alex <alexsandrodp1@gmail.com>
2023-11-08 22:27:07 -0300

    Classe Programa iniciada

--- Exercícios_de_fixação/src/application/Program.java
+++ Exercícios_de_fixação/src/application/Program.java
@@ -1,15 +1,40 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		/*Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois, mostrar um
 sumário do pedido conforme exemplo (próxima página). Nota: o instante do pedido deve ser
 o instante do sistema: new Date()*/
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-
+		System.out.println("Enter cliente data: ");
+		System.out.print("Name:");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.println("Birth date (DD/MM/YYYY):");
+		Date birthdate = sdf.parse(sc.next());
+		Client client = new entities.Client(name, email, birthdate); 
+		
+		System.out.print("Enter order data: ");
+		
+		
+		
+		sc.close();
 	}
 
 }
